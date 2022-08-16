38e152d65f4b2a89ebd17571a811558893f23905
RevonZev <senoaji2002@gmail.com>
2022-08-16 17:49:39 +0700

    Update all falling box when settings is updated
    ... instead of just new falling box that had changed leaving the existing falling boxes the same color as before the settings is changed

--- falling_writer/scripts/falling_lable_box.gd
+++ falling_writer/scripts/falling_lable_box.gd
@@ -1,9 +1,9 @@
 extends RigidBody2D
 
 
-func _ready():
-	$ColorRect.color = Color(UserSettings.get_setting("Falling box color"))
-	$Label.modulate = Color(UserSettings.get_setting("Falling box text color"))
+func _ready():	
+	var _err = UserSettings.connect("settings_written", self, "_settings_changed")
+	_settings_changed()
 
 
 func _on_Timer_timeout() -> void:
@@ -12,3 +12,8 @@ func _on_Timer_timeout() -> void:
 
 func _on_VisibilityNotifier2D_viewport_exited(_viewport: Viewport) -> void:
 	queue_free()
+	
+
+func _settings_changed():
+	$ColorRect.color = Color(UserSettings.get_setting("Falling box color"))
+	$Label.modulate = Color(UserSettings.get_setting("Falling box text color"))
