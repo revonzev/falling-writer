70b7d7f5e9b1d1cdfc25283b667dec958abadd14
RevonZev <senoaji2002@gmail.com>
2022-08-26 17:20:01 +0700

    Explosion animation & sound

--- falling_writer/scripts/falling_lable_box.gd
+++ falling_writer/scripts/falling_lable_box.gd
@@ -1,6 +1,9 @@
 extends RigidBody2D
 
 onready var explosion_area: Area2D = $Explosion/Area2D
+onready var explosion_sprite: Sprite = $Explosion/Sprite
+onready var explosion_animation: AnimationPlayer = $Explosion/AnimationPlayer
+onready var explosion_sound: AudioStreamPlayer2D = $Explosion/AudioStreamPlayer2D
 
 
 func _ready():
@@ -16,9 +19,19 @@ func explode():
 	for body in bodies_in_explosion:
 		var direction = (body.position - self.position).normalized()
 		var knockback = 1000 * (1 - clamp(body.position.distance_to(self.position) / 150, 0, 1))
-		body.set_linear_velocity(direction * knockback)
+		body.linear_velocity += direction * knockback
 
-	self.queue_free()
+	explosion_sound.play()
+	$Timer.paused = true
+
+	explosion_sprite.rotation = randi() % 360 + 1
+	explosion_animation.play("Explosion")
+
+
+func hide_box():
+	$ColorRect.hide()
+	$Label.hide()
+	$LightOccluder2D.hide()
 
 
 func _on_Timer_timeout() -> void:
